d77e4ea6f8d809e95e1d62c428e2515f92e99a47
Jihyuk Lee <lmh4686@gmail.com>
2022-09-10 15:07:54 +1000

    Cow Say

---
+++ main.py
@@ -0,0 +1,9 @@
+import modules.greet as greet 
+import random
+
+
+
+greet.hello('Lee')
+greet.hello('anc')
+print(greet.hello_count)
+print(dir(greet))
---
+++ modules/greet.py
@@ -0,0 +1,11 @@
+hello_count = 0
+
+def hello(name):
+    global hello_count
+    print(f"hello, {name}!")
+    hello_count += 1
+
+def goodbye(name):
+    print(f"Goodbye, {name}!")
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13]
